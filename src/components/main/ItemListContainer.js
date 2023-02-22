ec2a862f3b79bfa695ee884ef48812ba5927e605
Rodrigo Zerrezuela <rzerrezuela@gmail.com>
2023-02-22 15:32:20 -0300

    ItemCount Components

--- src/components/main/ItemListContainer.js
+++ src/components/main/ItemListContainer.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import ItemCount from '../ItemCount/ItemCount';
 import './ItemListCintainer.css';
 
-const ItemListContainer = ({msg}) => {
+const ItemListContainer = ({ msg }) => {
+
+  const onAdd =(count) =>{
+    console.log("el usuario selcciono ${count}")
+  }
+
   return (
-    <h1>{msg}</h1>
+    <>
+      <h1>{msg}</h1>
+      <ItemCount stock={5} onAdd={onAdd} />
+    </>
   )
 }
 
