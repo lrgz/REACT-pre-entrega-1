c7db368626b65f9a009c4657e20e5af3e881599e
Rodrigo Zerrezuela <rzerrezuela@gmail.com>
2023-02-22 20:20:53 -0300

    Tarea 2

---
+++ src/components/ItemList/ItemList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Item from '../Item/Item'
+
+
+const ItemList = ({products}) => {
+  return (
+    <>
+        {products.map((product) =>{
+                return <Item key={product.id} product={product}/>
+        })}
+    </>    
+  )
+}
+
+export default ItemList
